b2ff8d90c1bbb359028227787df47c9f9a2e83fc
LinuxMinrJulaicar <julian.david.sanchez@correounivalle.edu.co>
2019-09-17 18:30:33 -0500

    Tarea

--- tareaHAProxy/haproxy.sh
+++ tareaHAProxy/haproxy.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get install -y haproxy --fix-missing
+sudo git clone https://github.com/JulianDavidSanchez/archivos.git
+cd archivos
+sudo mv haproxy /etc/default/
+sudo mv /etc/haproxy/haproxy.cfg{,.original}
+sudo mv haproxy.cfg /etc/haproxy/
+sudo service haproxy start
+sudo service haproxy restart
